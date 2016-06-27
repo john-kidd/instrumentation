39072efa3b5db134249f3c26be78667d0e95d42d
john-kidd <john_kidd@outlook.com>
2016-06-27 21:51:59 +0100

    added models and updated queries

--- console apps/python/main.py
+++ console apps/python/main.py
@@ -4,10 +4,17 @@ from model import Contact
 from validators import validate_input
 from logger import log_info
 from execute_around_querie import timer
+from queries import GetContact
+from model import ContactId
 
 c = Contact("John F Kidd", "+4478866662", "john@test.com")
 
 validate_input(log_info, c)
+
+contact_id = ContactId(12345)
+get_contact = GetContact()
 query = timer(log_info)
 
-query(lambda: "test", lambda: "test 1")
+contact_12345 = query(get_contact.with_contact_id(contact_id).query, get_contact.get_description)
+
+log_info(contact_12345)
--- console apps/python/queries.py
+++ console apps/python/queries.py
@@ -1,7 +1,20 @@
-
+from model import Contact
 
 class Query:
 
     def get_description(self):
 
-        return __cl
+        return self.__class__.__name__
+
+
+class GetContact(Query):
+
+    def with_contact_id(self, contact_id):
+        self.contact_id = contact_id
+        return self
+
+
+    def query(self):
+
+        contact = Contact("John F Kidd", "+4478866662", "john@test.com")
+        return contact
