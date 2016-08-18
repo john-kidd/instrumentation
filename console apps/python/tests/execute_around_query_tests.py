9d816c3fb71818673800e9a56c6b36c9872d6db0
John F Kidd <john.kidd@flexitechsolution.co.uk>
2016-08-18 12:25:32 +0100

    updated execute around query tests

--- console apps/python/tests/execute_around_query_tests.py
+++ console apps/python/tests/execute_around_query_tests.py
@@ -7,15 +7,32 @@ from ..app.console_logger import log_info
 class ExecuteAroundQueryTestCase(unittest.TestCase):
     """"Tests for 'execute_around_query.py'"""
 
+    def setUp(self):
+        self.current_message = ""
+
     def test_timer_logs_duration(self):
+        query = timer(self._log_info)
+        query(lambda: "test", lambda: "test description")
+        self.assertIn("DURATION", self.current_message)
+
+    def test_timer_returns_expected_value(self):
         query = timer(log_info)
         result = query(lambda: "test", lambda: "test description")
         self.assertEqual("test", result)
 
-    def test_around_logs_duration(self):
+    def test_around_logs_before_and_after(self):
+        query = around(self._log_info)
+        query(lambda: "test", lambda: "test description")
+        self.assertIn("BEGIN", self.current_message)
+        self.assertIn("END", self.current_message)
+
+    def test_around_returns_expected_value(self):
         query = around(log_info)
         result = query(lambda: "test", lambda: "test description")
         self.assertEqual("test", result)
+
+    def _log_info(self, message):
+        self.current_message = self.current_message + message
 
 
 if __name__ == '__main__':
