86582b502d0638fa28a64a4612663a2802b3d489
John F Kidd <john.kidd@flexitechsolution.co.uk>
2016-08-29 12:31:18 +0100

    removed old code

--- console apps/python/app/queries.py
+++ console apps/python/app/queries.py
@@ -4,32 +4,6 @@ from ..app.shared.guard import validate_input, validate_output
 import db
 
 
-class Query:
-    """ Query is a base class that simply provides a get_description function """
-
-    def __init__(self):
-        pass
-
-    def get_description(self):
-        return self.__class__.__name__
-
-
-class GetContact(Query):
-    """ GetContact is a query that validates we have a contact_id and class a db to retrieve a matching contact """
-
-    def __init__(self, a_db=None):
-        if a_db is not None:
-            self.db = a_db
-        else:
-            self.db = db
-
-    def query(self, contact_id):
-        validate_input(log_info, contact_id)
-        contact = self.db.get_contact_by_id(contact_id)
-        validate_output(log_info, contact)
-        return contact
-
-
 def get_description():
     return "get_contact"
 
